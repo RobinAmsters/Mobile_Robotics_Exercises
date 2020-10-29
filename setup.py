aaf233ffbc07e07b3af468eea4cc7a559a2fc559
Robin Amsters <robin.amsters@kuleuven.be>
2020-10-29 14:30:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MobileRoboticsGT",
-    version="0.0.1",
+    version="0.0.2",
     author="Robin Amsters",
     author_email="robin.amsters@kuleuven.be",
     description="A Files related to the (online) exercises sessions from the mobile robotics course at KU Leuven, campus group T",
